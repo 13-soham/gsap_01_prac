ca73790aa74df5a779fc04acab0ed5bd9db50c3f
Soham Mondal <sohammondal821@gmail.com>
2024-07-27 13:00:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+var gp = gsap.timeline()
+
+gp.to("#box1", {
+    x : "70vw",
+    duration : 3,
+    delay : 1,
+    scale : 0.7,
+    rotate : "360deg",
+    backgroundColor : "limegreen",
+    textContent : "c"
+})
+gp.to("#box2", {
+    x : "70vw",
+    duration : 3,
+    scale : 0.6,
+    rotate : "360deg",
+    backgroundColor : "yellow",
+    textContent : "a"
+})
+gp.to("#box3", {
+    x : "70vw",
+    duration : 3,
+    scale : 0.5,
+    rotate : "360deg",
+    backgroundColor : "royalblue",
+    textContent : "b"
+})
+
